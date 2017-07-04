2c967cbaf6fcbcb295a0ce2613bbede9e0793727
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-07-04 21:41:30 +0200

    Only do word counts

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,7 @@ use std::env;
 
 #[inline]
 fn word_boundary(c: u8) -> bool {
-    c == b'\r' || c == b'\t' || c == b' '
-}
-
-#[inline]
-fn line_boundary(c: u8) -> bool {
-    c == b'\n'
+    c == b' ' || c == b'\r' || c == b'\t' || c == b'\n'
 }
 
 fn main() {
@@ -19,16 +14,11 @@ fn main() {
     }
     let name = args.nth(1).unwrap();
     let map = memmap::Mmap::open_path(&name, memmap::Protection::Read).unwrap();
-    let mut byte_count = 0;
     let mut word_count = 0;
-    let mut line_count = 0;
     for b in unsafe { map.as_slice() } {
-        byte_count += 1;
-        if line_boundary(*b) {
-            line_count += 1;
-        } else if word_boundary(*b) {
+        if word_boundary(*b) {
             word_count += 1;
         }
     }
-    println!("{} {} {} {}", line_count, word_count, byte_count, &name);
+    println!("{} {}", word_count, &name);
 }
